32e383473404bcbaedfc59b06b9c94b1cd92b3e5
dedEgor <egorowllebedev@gmail.com>
2024-03-06 12:40:50 +0300

    created main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,9 @@ function Main() {
             <div className={styles.container}>
                 <div className={styles.left} style={{backgroundImage: "url('/assets/duolingo.svg')"}}></div>
                 <div className={styles.right}>
-                    
+                    <p>Учите языки бесплатно, весело и эффективно!</p>
+                    <div className={styles.startBtn}>НАЧАТЬ</div>
+                    <div className={styles.alreadyRegisteredBtn}>У МЕНЯ УЖЕ ЕСТЬ АККАУНТ</div>
                 </div>
             </div>
         </div>
