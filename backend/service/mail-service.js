76218d779584d70af46e4f3b4ca9786d2e824de5
dedEgor <egorowllebedev@gmail.com>
2025-05-24 16:32:41 +0300

    fixed .env leak

--- backend/service/mail-service.js
+++ backend/service/mail-service.js
@@ -1,32 +1,32 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 class mailService {
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: "smtp.yandex.ru",
-            port: "465",
-            secure: true,
-            auth: {
-                user: "gogahahafunny@yandex.ru",
-                pass: "fxupnlyvdgoidbyq",
-            },
-        });
-    }
+	constructor() {
+		this.transporter = nodemailer.createTransport({
+			host: process.env.SMTP_HOST,
+			port: process.env.SMTP_PORT,
+			secure: true,
+			auth: {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASSWORD,
+			},
+		});
+	}
 
-    async sendActivationMail(email, link) {
-        await this.transporter.sendMail({
-            from: "gogahahafunny@yandex.ru",
-            to: email,
-            subject: "Активация аккаунта gogulingo",
-            text: "",
-            html: `
+	async sendActivationMail(email, link) {
+		await this.transporter.sendMail({
+			from: 'gogahahafunny@yandex.ru',
+			to: email,
+			subject: 'Активация аккаунта gogulingo',
+			text: '',
+			html: `
             <div>
                 <h1>Для активации перейдите по ссылке</h1>
                 <a href="${link}">${link}</a>
             </div>
             `,
-        });
-    }
+		});
+	}
 }
 
 export default new mailService();
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Footer from "./components/Footer/Footer";
-import Login from "./components/Login/Login";
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
+import Login from './components/Login/Login';
 
-import { useActions } from "./hooks/useActions";
-import { useTypedSelector } from "./hooks/useTypedSelector";
+import { useActions } from './hooks/useActions';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 const App: React.FC = () => {
-    const { ChangeScroll } = useActions();
-    const isLoginActive = useTypedSelector((state) => state.modals.login);
+	const { ChangeScroll } = useActions();
+	const isLoginActive = useTypedSelector((state) => state.modals.login);
 
-    const [currentLoginType, setCurrentLoginType] = useState("");
+	const [currentLoginType, setCurrentLoginType] = useState('');
 
-    const handleScroll = () => {
-        const position: number = window.scrollY;
-        ChangeScroll(position);
-    };
+	const handleScroll = () => {
+		const position: number = window.scrollY;
+		ChangeScroll(position);
+	};
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll, { passive: true });
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
-    return (
-        <div style={{ overflowY: isLoginActive ? "hidden" : "visible", height: isLoginActive ? "100vh" : "fit-content" }}>
-            <Header />
-            <Home setCurrentLoginType={setCurrentLoginType} />
-            <About />
-            <Footer />
-            {isLoginActive ? <Login setCurrentLoginType={setCurrentLoginType} currentLoginType={currentLoginType} /> : ""}
-        </div>
-    );
+	return (
+		<div
+			style={{ overflowY: isLoginActive ? 'hidden' : 'visible', height: isLoginActive ? '100vh' : 'fit-content' }}
+		>
+			<Header />
+			<Home setCurrentLoginType={setCurrentLoginType} />
+			<About />
+			<Footer />
+			{isLoginActive ? (
+				<Login setCurrentLoginType={setCurrentLoginType} currentLoginType={currentLoginType} />
+			) : (
+				''
+			)}
+		</div>
+	);
 };
 
 export default App;
